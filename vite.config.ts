3ce7b49564cf3e031af3c74457501fe2e55bfcd3
Webis-2022 <verjovkin.jevgeni@gmail.com>
2026-02-18 11:04:26 +0200

    chore: install and set up prettier, husky, vite, vitest, sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import eslint from "vite-plugin-eslint2";
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-export default defineConfig({
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint2';
+
+export default defineConfig(() => ({
   plugins: [eslint()],
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+  },
+}));
