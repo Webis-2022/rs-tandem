9009a2659c2c0b62fb1b4f185d3203c21ebfd048
Webis-2022 <verjovkin.jevgeni@gmail.com>
2026-02-18 12:19:50 +0200

    chore: create project files structure add fonts and normilize.css

--- src/math.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { sum } from './math';
-
-describe('Math utils', () => {
-  it('должна складывать числа', () => {
-    const result = sum(1, 2) as number;
-    expect(result).toBe(5);
-  });
-});
