f404970c1a56190a8ba29d6872a932c95048bc85
Webis-2022 <verjovkin.jevgeni@gmail.com>
2026-03-14 09:16:18 +0200

    test: install packages for tests and write a few tests

---
+++ src/components/ui/practice-card/side-panel/side-panel.test.ts
@@ -0,0 +1,41 @@
+import { createSidePanel } from './side-panel';
+import { screen } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
+import { createEl } from '../../../../shared/dom';
+import { showSidePanel } from '../../../../shared/show-side-panel';
+
+describe('createSidePanel', () => {
+  let sidePanel: HTMLDivElement | null;
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div class="page"></div>
+      <div class="card" style="height: 100px;"></div>
+    `;
+    createSidePanel();
+    sidePanel = document.querySelector('.side-panel');
+  });
+  test('createSidePanel adds side panel', () => {
+    expect(sidePanel).toBeInTheDocument();
+  });
+  test('side panel has correct children', () => {
+    const closeButton = screen.getByRole('button', { name: 'X' });
+    expect(closeButton).toBeInTheDocument();
+
+    const title = screen.getByText('Explanation');
+    expect(title).toBeInTheDocument();
+  });
+  test('contains className opened', async () => {
+    const button = createEl('div', {
+      text: "I don't know",
+      className: 'hint-btn',
+    });
+    document.body.append(button);
+
+    button.addEventListener('click', showSidePanel);
+    const btn = screen.getByText("I don't know");
+
+    await userEvent.click(btn);
+
+    expect(sidePanel).toHaveClass('opened');
+  });
+});
