7cf0f173857d4837ee284c5d5ee9851d2e5c14a1
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2026-03-25 16:25:01 +0200

    fix: sort topics by id in getTopics

--- src/services/api/get-topics.ts
+++ src/services/api/get-topics.ts
@@ -3,7 +3,10 @@ import { withApiErrorHandling } from '../../shared/helpers/request-error.ts';
 
 export async function getTopics() {
   return withApiErrorHandling(async () => {
-    const { data, error } = await supabase.from('topics').select('*');
+    const { data, error } = await supabase
+      .from('topics')
+      .select('*')
+      .order('id', { ascending: true });
 
     if (error) {
       throw error;
