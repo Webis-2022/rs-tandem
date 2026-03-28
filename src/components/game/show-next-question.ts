740b4bf7276c5f1b5aa1e78f0b9da649df90d384
Webis-2022 <verjovkin.jevgeni@gmail.com>
2026-03-28 21:22:42 +0200

    refactor: remove unnecessary function

--- src/components/game/show-next-question.ts
+++ src/components/game/show-next-question.ts
@@ -44,7 +44,6 @@ export async function showNextQuestion() {
     } else {
       toggleButtonsStatement('allButtons');
       await finishCurrentGame();
-      toggleButtonsStatement();
       return;
     }
   }
