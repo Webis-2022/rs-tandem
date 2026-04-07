8030caa3e3682d40c3d7cf508911c9a8c9b04671
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2026-04-07 17:33:45 +0300

    fix: restore used hints after page refresh

--- src/components/ui/practice-card/hints-container/hints-container.ts
+++ src/components/ui/practice-card/hints-container/hints-container.ts
@@ -3,22 +3,40 @@ import { showSidePanel } from '../../../game/hintsLogic/show-side-panel';
 import { removeTwoWrongAnswers } from '../../../game/hintsLogic/remove-two-wrong-answers';
 import './hints-container.scss';
 import { callFriend } from '../../../game/hintsLogic/call-friend';
+import { getState } from '../../../../app/state/store';
+import type { HintCounter } from '../../../../types';
 
 type HintConfig = {
-  [key: string]: [(e: MouseEvent) => void, string];
+  [key: string]: [(e: MouseEvent) => void, string, keyof HintCounter];
 };
 
 export function createHintsContainer() {
+  const usedHints = getState().game.usedHints;
+
   const hintButtons: HintConfig = {
-    '50/50': [removeTwoWrongAnswers, 'fifty-fifty'],
-    'Call a friend': [callFriend, 'friend'],
-    "I don't know": [(e: MouseEvent) => showSidePanel(e), 'i-dont-know'],
+    '50/50': [removeTwoWrongAnswers, 'fifty-fifty', '50/50'],
+    'Call a friend': [callFriend, 'friend', 'call a friend'],
+    "I don't know": [
+      (e: MouseEvent) => showSidePanel(e),
+      'i-dont-know',
+      "i don't know",
+    ],
   };
+
   const container = createEl('div', { className: 'hints-container' });
-  Object.entries(hintButtons).forEach(([buttonKey, [handler, className]]) => {
-    const hintButton = createButton(buttonKey, handler, className);
-    hintButton.classList.add('hint-btn');
-    container.append(hintButton);
-  });
+
+  Object.entries(hintButtons).forEach(
+    ([buttonKey, [handler, className, hintKey]]) => {
+      const hintButton = createButton(buttonKey, handler, className);
+      hintButton.classList.add('hint-btn');
+
+      if ((usedHints?.[hintKey] ?? 0) > 0) {
+        hintButton.disabled = true;
+      }
+
+      container.append(hintButton);
+    }
+  );
+
   return container;
 }
