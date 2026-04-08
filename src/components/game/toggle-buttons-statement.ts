62f041d614f76bf1f58355a37547ab898be33db8
Webis-2022 <verjovkin.jevgeni@gmail.com>
2026-04-08 12:59:30 +0300

    refactor: add animation to buttons and replace document title

--- src/components/game/toggle-buttons-statement.ts
+++ src/components/game/toggle-buttons-statement.ts
@@ -11,7 +11,9 @@ export function toggleButtonsStatement(
     hintButtons.forEach((button) => button.setAttribute('disabled', ''));
     checkButton?.setAttribute('disabled', '');
     nextTopicButton?.removeAttribute('disabled');
+    nextTopicButton?.classList.add('animated-button');
     libraryButton?.removeAttribute('disabled');
+    libraryButton?.classList.add('animated-button');
   } else if (mode === 'oneButton') {
     if (!className) return;
     const button = document.querySelector<HTMLButtonElement>(className);
