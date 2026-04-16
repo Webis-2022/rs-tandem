3b248538a2ea1b7806da0bfda52e32fc82c109e8
AleksPanda <149190602+AleksPanda@users.noreply.github.com>
2026-04-16 19:15:08 +0300

    refactor: normalize file naming to kebab-case

--- src/pages/practice/practice.ts
+++ src/pages/practice/practice.ts
@@ -1,7 +1,7 @@
 import { saveTopicQuestions } from '../../app/state/actions';
 import { getState } from '../../app/state/store';
 import { showNextQuestion } from '../../components/game/show-next-question';
-import { updateScore } from '../../components/game/updateScore';
+import { updateScore } from '../../components/game/update-score';
 import { createLoadingView } from '../../components/ui/loading/loading';
 import { createPracticeCard } from '../../components/ui/practice-card/practice-card';
 import { createSidePanel } from '../../components/ui/practice-card/side-panel/side-panel';
